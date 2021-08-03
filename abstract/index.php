57aeb8226ff30085307c8cd5e0da892b61bf6d48
rikkimuga <85866491+rikkimuga@users.noreply.github.com>
2021-08-03 21:23:33 +0300

    Add files via upload

---
+++ abstract/index.php
@@ -0,0 +1,105 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>User</title>
+    <script src="bootstrap/js/jquery-3.4.0.js"></script>
+    <script src="bootstrap/js/bootstrap.js"></script>
+    <script src="bootstrap/js/bootstrap.js"></script>
+
+    <script
+            src="https://use.fontawesome.com/releases/v5.15.3/js/all.js"
+            data-auto-a11y="true"
+    ></script>
+
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.css">
+    <link rel="stylesheet" href="bootstrap/css/stylesheet.css">
+
+</head>
+<body>
+<div class="row1">
+    <nav class="navbar navbar-expand-sm bg-dark navbar-dark ">
+
+        <ul class=" navbar-nav ">
+            <li class="nav-item active ">
+                <a class="nav-link logo" href="#">Abstract</a>
+            </li>
+
+            <li class="nav-item">
+                <a class="nav-link r a" href="#">Home</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link a" href="#">Services</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link  a" href="#">Blog</a>
+
+            </li>
+
+        </ul>
+        <form class="form-inline " action="/action_page.php">
+            <input class="  form-control mr-sm-2 " type="text" placeholder="Search for item or brand">
+            <button class="btn btn-success"  type="submit">Search </button>
+        </form>
+    </nav>
+
+
+                      <div class="text-white text-center">
+                          <br><br>
+                          <i class="fa fa-shopping-bag" aria-hidden="true"></i>
+                          <br><br>
+                          <h1>ABSTRACT</h1><br>
+                          <br>
+                          <br>
+                          <h3>BUY | SELL | DISCOVER YOUR OWN STYLE</h3><br>
+                          <br>
+                          <br><br>
+                       </div>
+                      </div>
+                          <br><br>
+
+
+
+
+</div>
+<br>
+<br>
+<div class="row">
+    <div class="col"><img class="img1" src="resources/img1.webp" alt=""></div>
+    <div class="col ">
+        <br>
+        <br>
+        <h2>Discover your <br>style</h2>
+    </div>
+
+</div>
+<div class="row">
+    <div class="col">
+        <br>
+        <br>
+        <h2 class="wow">Reuse and <br>
+        Recycle items</h2>
+
+    </div>
+    <br>
+    <br>
+    <div class="col ">
+        <br>
+        <br>
+        <img class="bob" src="resources/mywebsite6.webp" alt="">
+    </div>
+
+</div>
+<div class="row">
+<div class="col">
+    <img class="img1" src="resources/mywebsite5.webp" alt="">
+</div>
+<div class="col ">
+    <br>
+    <br>
+    <h2>Discover your <br>style</h2>
+</div>
+</body>
